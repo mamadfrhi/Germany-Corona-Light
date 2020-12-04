a9a4711f0ea63435923189fe07a24c8419e573ab
iMamad <mamad.frhi@gmail.com>
2020-12-04 18:10:22 +0330

    Initial Commit

---
+++ Corona Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Corona Light
+//
+//  Created by iMamad on 12/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
